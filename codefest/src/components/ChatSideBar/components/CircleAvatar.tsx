1b8c245e0009dea97016e38ecb9b99e03b07ccd9
LuxyKolink <santiagolikeaboss22@gmail.com>
2023-10-25 09:47:23 -0500

    circle avatar

---
+++ codefest/src/components/ChatSideBar/components/CircleAvatar.tsx
@@ -0,0 +1,9 @@
+const  CircleAvatar = () => {
+    return (
+        <button className="button button5">50%
+            <img src="" alt="">buttonpng</img>
+        </button>
+    );
+};
+
+export default CircleAvatar;
