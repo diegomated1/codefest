eae27e925dea7e79fb2f895cbc0c06f8e604803c
unknown <kevin.quintana.256bit@gmail.com>
2023-10-25 13:07:19 -0500

    terminaod el chat

--- codefest/src/components/Chat.tsx
+++ codefest/src/components/Chat.tsx
@@ -1,4 +1,7 @@
 import { FC } from "react";
+import ChatComponent from "./ChatComponent";
+import Input from "./Input";
+import Button from "./Button";
 
 type ChatComponentType = {
   nombre: string;
@@ -6,140 +9,32 @@ type ChatComponentType = {
 
 const Chat: FC<ChatComponentType> = ({ nombre }) => {
   return (
-    <section>
-      <div className="container py-5">
-        <div className="row d-flex justify-content-center">
-          <div className="col-md-10 col-lg-8 col-xl-6">
-            <div className="card" id="chat2">
-              <div className="card-header d-flex justify-content-between align-items-center p-3">
-                <h5 className="mb-0">GRUPO DE LA COMBIBENCIA</h5>
+    <section className="w-100 h-100">
+      <div className=" py-3 w-100 h-100">
+        <div className="w-100 h-100">
+          <div className="w-100 h-100">
+            <div className="card  w-100 h-100" id="chat2">
+              <div className="card-header align-items-center p-3">
+                <h5 className="mb-0">{nombre}</h5>
               </div>
-              <div className="card-body" data-mdb-perfect-scrollbar="true">
-                <div className="d-flex flex-row justify-content-start">
+              <div className="card-body bg-light" data-mdb-perfect-scrollbar="true">
+                <div className="d-flex flex-row justify-content-end mb-4 pt-1 ">
                   <div>
-                    <p className="small p-2 ms-3 mb-1 rounded-3">Hi</p>
-                    <p className="small p-2 ms-3 mb-1 rounded-3">
-                      How are you ...???
-                    </p>
-                    <p className="small p-2 ms-3 mb-1 rounded-3">
-                      What are you doing tomorrow? Can we come up a bar?
-                    </p>
-                    <p className="small ms-3 mb-3 rounded-3 text-muted">
-                      23:58
-                    </p>
-                  </div>
-                </div>
-
-                <div className="divider d-flex align-items-center mb-4">
-                  <p className="text-center mx-3 mb-0">Today</p>
-                </div>
-
-                <div className="d-flex flex-row justify-content-end mb-4 pt-1">
-                  <div>
-                    <p className="small p-2 me-3 mb-1 text-white rounded-3 bg-primary">
-                      Hiii, I'm good.
-                    </p>
-                    <p className="small p-2 me-3 mb-1 text-white rounded-3 bg-primary">
-                      How are you doing?
-                    </p>
-                    <p className="small p-2 me-3 mb-1 text-white rounded-3 bg-primary">
-                      Long time no see! Tomorrow office. will be free on sunday.
-                    </p>
-                    <p className="small me-3 mb-3 rounded-3 text-muted d-flex justify-content-end">
-                      00:06
-                    </p>
-                  </div>
-                </div>
-
-                <div className="d-flex flex-row justify-content-start mb-4">
-                  <div>
-                    <p className="small p-2 ms-3 mb-1 rounded-3">Okay</p>
-                    <p className="small p-2 ms-3 mb-1 rounded-3">
-                      We will go on Sunday?
-                    </p>
-                    <p className="small ms-3 mb-3 rounded-3 text-muted">
-                      00:07
-                    </p>
-                  </div>
-                </div>
-
-                <div className="d-flex flex-row justify-content-end mb-4">
-                  <div>
-                    <p className="small p-2 me-3 mb-1 text-white rounded-3 bg-primary">
-                      That's awesome!
-                    </p>
-                    <p className="small p-2 me-3 mb-1 text-white rounded-3 bg-primary">
-                      I will meet you Sandon Square sharp at 10 AM
-                    </p>
-                    <p className="small p-2 me-3 mb-1 text-white rounded-3 bg-primary">
-                      Is that okay?
-                    </p>
-                    <p className="small me-3 mb-3 rounded-3 text-muted d-flex justify-content-end">
-                      00:09
-                    </p>
-                  </div>
-                </div>
-
-                <div className="d-flex flex-row justify-content-start mb-4">
-                  <div>
-                    <p className="small p-2 ms-3 mb-1 rounded-3">
-                      Okay i will meet you on Sandon Square
-                    </p>
-                    <p className="small ms-3 mb-3 rounded-3 text-muted">
-                      00:11
-                    </p>
-                  </div>
-                </div>
-
-                <div className="d-flex flex-row justify-content-end mb-4">
-                  <div>
-                    <p className="small p-2 me-3 mb-1 text-white rounded-3 bg-primary">
-                      Do you have pictures of Matley Marriage?
-                    </p>
-                    <p className="small me-3 mb-3 rounded-3 text-muted d-flex justify-content-end">
-                      00:11
-                    </p>
-                  </div>
-                </div>
-
-                <div className="d-flex flex-row justify-content-start mb-4">
-                  <div>
-                    <p className="small p-2 ms-3 mb-1 rounded-3">
-                      Sorry I don't have. i changed my phone.
-                    </p>
-                    <p className="small ms-3 mb-3 rounded-3 text-muted">
-                      00:13
-                    </p>
-                  </div>
-                </div>
-
-                <div className="d-flex flex-row justify-content-end">
-                  <div>
-                    <p className="small p-2 me-3 mb-1 text-white rounded-3 bg-primary">
-                      Okay then see you on sunday!!
-                    </p>
-                    <p className="small me-3 mb-3 rounded-3 text-muted d-flex justify-content-end">
-                      00:15
-                    </p>
+                    <ChatComponent
+                      nombre={"Kevinsito"}
+                      mensage={"QUELAQUEHAY perros hptas"}
+                    />
+                    <ChatComponent
+                      nombre={"Kevinsito"}
+                      mensage={"QUELAQUEHAY perros hptas"}
+                    />
                   </div>
                 </div>
               </div>
               <div className="card-footer text-muted d-flex justify-content-start align-items-center p-3">
-                <input
-                  type="text"
-                  className="form-control form-control-lg"
-                  id="exampleFormControlInput1"
-                  placeholder="Type message"
-                />
-                <a className="ms-1 text-muted" href="#!">
-                  <i className="fas fa-paperclip"></i>
-                </a>
-                <a className="ms-3 text-muted" href="#!">
-                  <i className="fas fa-smile"></i>
-                </a>
-                <a className="ms-3" href="#!">
-                  <i className="fas fa-paper-plane"></i>
-                </a>
+                <Input nombre={""}/>
+                <div style={{padding:"5px"}} />
+                <Button nombre={"Enviar"}/>
               </div>
             </div>
           </div>
--- codefest/src/pages/chatTeam/ChatTeam.tsx
+++ codefest/src/pages/chatTeam/ChatTeam.tsx
@@ -9,7 +9,7 @@ export const ChatTeam = () => {
     <section className="vh-100">
       <div className="container h-100">
         <div className="row d-flex justify-content-between h-100">
-            <form style={{ display: "flex" }}>
+            <form style={{width: "100%"}}>
                 <Chat nombre={"Grupo"}/>
             </form>
           </div>
